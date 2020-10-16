85046a3486fe77608b1c9f0f2caa85ce7d2cf5d9
ZetherDev <khiapola01@gmail.com>
2020-10-16 16:20:30 -0300

    Edit Game.php (add times - add border deathmatch - set times)

--- src/hg/game/Game.php
+++ src/hg/game/Game.php
@@ -2,6 +2,10 @@
 
 namespace hg\game;
 
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\Server;
+
 /**
  * Class Game
  * @package hg\game
@@ -9,12 +13,36 @@ namespace hg\game;
 class Game
 {
 
+    /** @var int */
+    const STATUS_WAITING = 0;
+    /** @var int */
+    const STATUS_STARTING = 1;
+    /** @var int */
+    const STATUS_RUNNING = 2;
+    /** @var int */
+    const STATUS_DEATHMATCH = 3;
+    /** @var int */
+    const STATUS_WINNER = 4;
+
     /** @var string */
     private $levelName;
     /** @var int */
     private $slots;
     /** @var array */
     private $spawns;
+
+    /** @var int */
+    private $status = self::STATUS_WAITING;
+
+    /** @var int */
+    private $cooldown = 30;
+    /** @var float|int */
+    private $time = 8 * 60;
+    /** @var int */
+    private $deathmatch = 10;
+
+    /** @var int */
+    private $border = 100;
 
     /**
      * Game constructor.
@@ -37,6 +65,11 @@ class Game
         return $this->levelName;
     }
 
+    public function getLevel(): Level
+    {
+        return Server::getInstance()->getLevelByName($this->getLevelName());
+    }
+
     /**
      * @return int
      */
@@ -51,5 +84,42 @@ class Game
     public function getSpawns(): array
     {
         return $this->spawns;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBorder(): int
+    {
+        return $this->border;
+    }
+
+    /**
+     * @param int $slot
+     * @return false|Vector3
+     */
+    public function getSpawn(int $slot = 1)
+    {
+        if (isset($this->spawns[$slot])) {
+            $spawn = $this->spawns[$slot];
+            return new Vector3($spawn[0], $spawn[1], $spawn[2]);
+        }
+        return false;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatus(int $status)
+    {
+        $this->status = $status;
     }
 }
